cb3d7fecf7b685317e142e997eb657aeff964701
Kenneth Martinez <krossmartinez@gmail.com>
2021-01-08 14:13:43 -0800

    General Game functionality working

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-playerChoices = ["Rock" , "Paper", "Scissor", "Lizard", "Spock"]
-console.log(choices[0])
+// playerChoices = ["Rock" , "Paper", "Scissor", "Lizard", "Spock"]
+// console.log(choices[0])
+// let userAns = "Rock";
+let cpuAns;
+
+
+
 
 let winCount = 0;
-let loseCoun = 0;
+let loseCount = 0;
 let tieCount = 0;
 
 
@@ -16,37 +21,106 @@ let btnLizard = document.getElementById("btnLizard");
 let btnSpock = document.getElementById("btnSpock");
 
 
-//This fetch grabs the api for CPU comparison 
+// This fetch grabs the api for CPU comparison 
 // fetch("https://csa2020studentapi.azurewebsites.net/rpsls").
 // then(data =>{
 //     data.text().
 //     then(value => console.log(value))
 // });
 
-async function cpuChoice(){
+async function getCPU() {
     let promise = await fetch("https://csa2020studentapi.azurewebsites.net/rpsls");
     cpuAns = await promise.text();
-    compare();
 }
 
+getCPU()
 
-btnRock.addEventListener('click', function(){
-    console.log("Rock")
+btnRock.addEventListener('click', function () {
+    compare("Rock")
+    getCPU()
+    //console.log(cpuAns)
+    
 })
 
-btnPaper.addEventListener('click', function(){
-    console.log("Paper")
+btnPaper.addEventListener('click', function () {
+    compare("Paper")
+    getCPU()
+    //console.log("Paper")
 })
 
-btnScissor.addEventListener('click', function(){
-    console.log("Scissor")
+btnScissor.addEventListener('click', function () {
+    compare("Scissors")
+    getCPU()
+    //console.log("Scissor")
 })
 
-btnLizard.addEventListener('click', function(){
-    console.log("Lizard")
+btnLizard.addEventListener('click', function () {
+    compare("Lizard")
+    getCPU()
+    //console.log("Lizard")
 })
 
-btnSpock.addEventListener('click', function(){
-    console.log("Spock")
+btnSpock.addEventListener('click', function () {
+    compare("Spock")
+    getCPU()
+    //console.log("Spock")
 })
 
+
+function compare(userAns) {
+    console.log(userAns)
+    console.log(cpuAns)
+    if (cpuAns == userAns) {
+        console.log("Tie! The same thing cannot beat itself you silly goose!")
+        tieCount++;
+    } else if (userAns == "Rock") {
+        if (cpuAns == "Paper" || cpuAns == "Spock") {
+            loseCount++;
+            console.log(`You Lose because you chose ${userAns} and the computer chose ${cpuAns}`)
+            
+        } else if (cpuAns == "Scissors" || cpuAns == "Lizard") {
+            winCount++;
+            console.log(`You win because you chose ${userAns} and the computer chose ${cpuAns}`)
+
+        }
+    } else if (userAns == "Paper") {
+        if (cpuAns == "Scissors" || "Lizard") {
+            loseCount++;
+            console.log(`You Lose because you chose ${userAns} and the computer chose ${cpuAns}`)
+
+        } else if (cpuAns == "Rock" || "Spock") {
+            wintCount++;
+            console.log(`You win because you chose ${userAns} and the computer chose ${cpuAns}`)
+        }
+    } else if (userAns == "Scissors") {
+        if (cpuAns == "Spock" || "Rock") {
+            loseCount++;
+            console.log(`You Lose because you chose ${userAns} and the computer chose ${cpuAns}`)
+
+        } else if (cpuAns == "Scissors" || "Paper") {
+            winCount++;
+            console.log(`You win because you chose ${userAns} and the computer chose ${cpuAns}`)
+        }
+    }
+    else if (userAns == "Lizard") {
+        if (cpuAns == "Rock" || "Scissors") {
+            loseCount++;
+            console.log(`You Lose because you chose ${userAns} and the computer chose ${cpuAns}`)
+
+        } else if (cpuAns == "Paper" || "Spock") {
+            winCount++;
+            console.log(`You win because you chose ${userAns} and the computer chose ${cpuAns}`)
+        }
+    } else if (userAns == "Spock") {
+        if (cpuAns == "Paper" || "Lizard") {
+            loseCount++;
+            console.log(`You Lose because you chose ${userAns} and the computer chose ${cpuAns}`)
+
+        } else if (cpuAns == "Rock" || "Scissors") {
+            winCount++;
+            console.log(`You win because you chose ${userAns} and the computer chose ${cpuAns}`)
+        }
+    } else {
+        console.log("We have an issue");
+    }
+}
